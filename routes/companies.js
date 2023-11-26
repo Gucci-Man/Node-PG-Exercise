0f7498ed47890086ae35d81836d2a82c7461355e
mrtomno <adel.ngo@yahoo.com>
2023-11-26 11:11:07 -0600

    added PATCH company route

--- routes/companies.js
+++ routes/companies.js
@@ -32,7 +32,7 @@ router.get('/:code', async (req, res, next) => {
 // POST /companies : Adds a company.
 router.post('/', async (req, res, next) => {
     try {
-        console.log(`code to be added is ${code}`);
+        const {code, name, description} = req.body;
         const results = await db.query('INSERT INTO companies (code, name, description) VALUES ($1, $2, $3) RETURNING code, name, description', 
         [code, name, description]);
         return res.status(201).json({ company: results.rows[0] })
@@ -41,5 +41,18 @@ router.post('/', async (req, res, next) => {
     }
   })
 
-
+// PUT /companies/[code] : Edit existing company.
+router.patch('/:code', async (req, res, next) => {
+    try {
+      const { code } = req.params;
+      const { name, description } = req.body;
+      const results = await db.query('UPDATE companies SET name=$1, description=$2 WHERE code=$3 RETURNING code, name, description', [name, description, code])
+      if (results.rows.length === 0) {
+        throw new ExpressError(`Can't update company with code of ${code}`, 404)
+      }
+      return res.send({ company: results.rows[0] })
+    } catch (e) {
+      return next(e)
+    }
+  })
   module.exports = router;
