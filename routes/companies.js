4af89f635d44f4c7eecf2164d06fd0d2bbb14146
mrtomno <adel.ngo@yahoo.com>
2023-11-26 11:03:48 -0600

    Added POST company request

--- routes/companies.js
+++ routes/companies.js
@@ -17,16 +17,29 @@ router.get('/', async (req, res, next) => {
 
 // GET /companies/[code] : Return obj of company:
 router.get('/:code', async (req, res, next) => {
-    const { code } = req.params;
     try {
-      const results = await db.query('SELECT * FROM companies WHERE code = $1', [code]);
-      if (results.rows.length === 0) {
-        throw new ExpressError(`Can't find company with code of ${code}`, 404)
-      }
-      return res.json({ companies: results.rows[0] })
+        const { code } = req.params;
+        const results = await db.query('SELECT * FROM companies WHERE code = $1', [code]);
+        if (results.rows.length === 0) {
+            throw new ExpressError(`Can't find company with code of ${code}`, 404)
+        }
+        return res.json({ companies: results.rows[0] })
     } catch (e) {
-      return next(e);
+        return next(e);
     }
   })
+
+// POST /companies : Adds a company.
+router.post('/', async (req, res, next) => {
+    try {
+        console.log(`code to be added is ${code}`);
+        const results = await db.query('INSERT INTO companies (code, name, description) VALUES ($1, $2, $3) RETURNING code, name, description', 
+        [code, name, description]);
+        return res.status(201).json({ company: results.rows[0] })
+    }   catch (e) {
+      return next(e)
+    }
+  })
+
 
   module.exports = router;
