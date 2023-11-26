356f4b41f1e34d45b81b47d875051112456585f9
mrtomno <adel.ngo@yahoo.com>
2023-11-26 12:16:53 -0600

    added PATCH and DELETE invoice requests

--- routes/invoices.js
+++ routes/invoices.js
@@ -13,7 +13,7 @@ router.get('/', async (req, res, next) => {
     } catch (e) {
       return next(e);
     }
-  })
+})
 
 // GET /invoices/[id] : Return obj of an invoice:
 router.get('/:id', async (req, res, next) => {
@@ -27,7 +27,45 @@ router.get('/:id', async (req, res, next) => {
     } catch (e) {
         return next(e);
     }
-  })
+})
 
+// POST /invoices : Adds an invoice. 
+router.post('/', async (req, res, next) => {
+    try {
+        const {comp_code, amt} = req.body;
+        const results = await db.query(
+            'INSERT INTO invoices (comp_code, amt) VALUES ($1, $2) RETURNING id, comp_code, amt, paid, add_date, paid_date',
+            [comp_code, amt]);
+        return res.status(201).json({ invoice: results.rows[0] })
+    }   catch (e) {
+      return next(e)
+    }
+})
+
+// PUT /invoices/[id] : Updates an invoice.
+router.patch('/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { amt } = req.body;
+        const results = await db.query('UPDATE invoices SET amt=$1 WHERE id=$2 RETURNING id, comp_code, amt, paid, add_date, paid_date', [amt, id])
+        if (results.rows.length === 0) {
+            throw new ExpressError(`Can't update invoice with id of ${id}`, 404)
+        }
+        return res.send({ invoice: results.rows[0] })
+        } catch (e) {
+        return next(e)
+    }
+})
+
+// DELETE /invoices/[id]
+router.delete('/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const results = db.query('DELETE FROM invoices WHERE id = $1', [id])
+        return res.send({ status: "deleted" })
+        } catch (e) {
+        return next(e)
+    }
+})
 
 module.exports = router;
