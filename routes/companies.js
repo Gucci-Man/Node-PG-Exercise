f12dea219f334959185a7e214bddce1d898011dd
mrtomno <adel.ngo@yahoo.com>
2023-11-26 10:37:59 -0600

    added Get obj company route

--- routes/companies.js
+++ routes/companies.js
@@ -5,10 +5,25 @@ const ExpressError = require("../expressError")
 const router = express.Router();
 const db = require("../db");
 
+// GET /companies : Returns list of companies
 router.get('/', async (req, res, next) => {
     try {
       const results = await db.query(`SELECT * FROM companies`);
       return res.json({ companies: results.rows })
+    } catch (e) {
+      return next(e);
+    }
+  })
+
+// GET /companies/[code] : Return obj of company:
+router.get('/:code', async (req, res, next) => {
+    const { code } = req.params;
+    try {
+      const results = await db.query('SELECT * FROM companies WHERE code = $1', [code]);
+      if (results.rows.length === 0) {
+        throw new ExpressError(`Can't find company with code of ${code}`, 404)
+      }
+      return res.json({ companies: results.rows[0] })
     } catch (e) {
       return next(e);
     }
